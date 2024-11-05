b432b6ada508ba1d3abe05cc266c629efafb1fdb
Jackson Casey <65034067+jaxcksn@users.noreply.github.com>
2024-11-04 20:18:50 -0600

    Add fallback for CORS

--- backend/server/main.py
+++ backend/server/main.py
@@ -28,7 +28,7 @@ app.include_router(administrator.router)
 # CORS
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=[os.getenv("FRONTEND_URL")],
+    allow_origins=[os.getenv("FRONTEND_URL", "http://localhost:5173")],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
