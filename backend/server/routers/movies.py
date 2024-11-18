a3d94f3ad9c4a1339851b658b59e50a44283a436
zack <zacksims129@gmail.com>
2024-11-18 15:42:51 -0600

    current implementation, doesnt work yet. Need to solve unknown error in backend then check for correctness

--- backend/server/routers/movies.py
+++ backend/server/routers/movies.py
@@ -3,9 +3,7 @@ from pydantic import BaseModel, Field
 from typing import Optional
 from ..util import DB, newId
 from ..dependencies import auth, admin
-
 router = APIRouter()
-
 
 
 # ---------------------------------------------------------------------------- #
@@ -31,8 +29,6 @@ async def movies(user_id: str = Depends(auth)):
         "SELECT id, title, release_date as showings_start, poster_url FROM MovieShowing"
     )
     return results
-
-
 
 @router.post("/movie/review", status_code=200)
 async def create_movie_review(
@@ -69,3 +65,65 @@ async def create_movie_review(
     )
 
     return {"message": "Review submitted successfully"}
+
+ 
+@router.get("/movie/{id}", status_code=200)
+async def fetchMovieInformation(
+    _id: str
+):
+    
+    
+
+    #get the MovieBooking entry and check for error
+    showingData = await DB.queryOne(
+        "SELECT * FROM `MovieShowing`       \
+        WHERE `id` = :_id",
+        {"_id": _id}
+    )
+
+    #if no data, then return error
+    if showingData is None:
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            detail="Provided ID doesn't match any database records"
+        )
+    
+    #get the MovieBooking entry and check for error
+    reviews = await DB.query(
+        "SELECT * FROM `Review`       \
+        WHERE `id` = :_id",
+        {"_id": _id}
+    )
+
+    #for did_review field
+    didReview = True
+    if reviews is None:
+        didReview = False
+
+    #format data for frontend 
+    result = {
+        "id": showingData[0],
+        "title": showingData[1],
+        "description": showingData[2],
+        "rating": showingData[4],
+        "runtime": showingData[3],
+        "cast": showingData[5],
+        "release_date": showingData[6],
+        "poster_url": showingData[7],
+        "reviews": [
+            {"published": review[0], "rating": review[1], "text": review[2]}
+            for review in reviews
+        ],
+        "did_review": didReview,
+        "showing": {
+            "start_date": showingData[8],
+            "end_date": showingData[9],
+            "show_times": [
+                {"string": data}
+                for data in showingData[10]
+            ],
+    "showing_id": showingData[11],
+    "price": 0
+  }
+}
+    return (result)
